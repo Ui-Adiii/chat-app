2a394ed3663f037291b805a447d6e949682868ae
Ui-Adiii <beaheroinyourlife@gmail.com>
2025-11-28 15:23:38 +0530

    adding cors

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,9 @@ const PORT = process.env.PORT || 5000
 app.use(cors({
     origin: process.env.FRONTEND_URL||'http://localhost:5173',
     methods:["POST","GET","PATCH","PUT","DELETE"],
-    credentials:true
+    credentials:true,
+    sameSite: "none",
+    secure: true
 }))
 
 const limiter = rateLimit({
