134d746144c4c26dcf082b5a49e04286effe0c9f
Aditya Mallick <147989750+Ui-Adiii@users.noreply.github.com>
2025-11-28 15:00:35 +0530

    update auth middleware

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -4,14 +4,14 @@ const authMiddleware = async (req,res,next)=> {
     try {
         const authToken = req.cookies?.auth_token;
         
-        if(!authToken) return response(req,401,'unauthorized user');
+        if(!authToken) return response(res,401,'unauthorized user');
         const decoded = jwt.verify(authToken,process.env.JWT_SECRET)
         
-        if(!decoded)return response(req,401,'unauthorized user');
+        if(!decoded)return response(res,401,'unauthorized user');
         req.user=decoded;
         next();
     } catch (error) {
-        response(req,401,error.message);
+        response(res,401,error.message);
     }
 }
 export default authMiddleware
